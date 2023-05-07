e0c2818b549a3286b74fcc4cae71c7293c18f317
Jhennifer Smith <jhennifersmithsoares@gmail.com>
2023-05-07 13:07:44 -0300

    Adicionando dados a DB

--- ScraperLexico.py
+++ ScraperLexico.py
@@ -1,6 +1,9 @@
+import mysql.connector
 import requests
 from bs4 import BeautifulSoup
-response = requests.get ('https://www.lexico.pt/amar/')
+
+palavra = input("Digite a palavra: " )
+response = requests.get ('https://www.lexico.pt/' + palavra)
 
 content = response.content
 site = BeautifulSoup(content, 'html.parser')
@@ -31,4 +34,30 @@ if len(antonimos) == 0:
 else:
     print(antonimos)
 
+mydb = mysql.connector.connect(
+  host="sql10.freemysqlhosting.net",
+  user="sql10612581",
+  password="Qhd7b3PWb4",
+  database="sql10612581",
+  port=3306
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM palavra WHERE nome = '" + palavra + "';")
+myresult = mycursor.fetchall()
+if len(myresult) == 0:
+    insert_palavra_sql = "INSERT INTO palavra (nome) VALUES ('" + palavra +"')"
+    mycursor.execute(insert_palavra_sql)
+    mydb.commit()
+    print("Palavra adicionada com sucesso")
+    id_palavra = mycursor.execute("SELECT idpalavra FROM palavra WHERE nome = '" + palavra + "';")
+    table_id = mycursor.fetchone()[0]
+    for sin in sinonimos:
+        insert_sinonimo_sql = "INSERT INTO sinonimo (nome, palavra_idpalavra) VALUES ('" + sin + "','" + id_palavra + "')"
+        mycursor.execute(insert_sinonimo_sql)
+        mydb.commit()
+    for ant in antonimos:
+        insert_antonimos_sql = "INSERT INTO antonimo (nome, palavra_idpalavra) VALUES ('" + ant + "','" + id_palavra + "')"
+        mycursor.execute(insert_antonimos_sql)
+        mydb.commit()
 
